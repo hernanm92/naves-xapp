eeebe7d148ced393fb9251670e0b0698756cadde
Hernan Maschwitz <hernanm992@gmail.com>
2015-06-04 00:28:39 -0300

    integracion con la api de youtube

---
+++ app/src/main/java/com/xapp/naves/xapp/ViewVideoActivity.java
@@ -0,0 +1,43 @@
+package com.xapp.naves.xapp;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class ViewVideoActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener{
+
+    public static final String API_KEY = "AIzaSyDzw0McyGses_csUu74l0R_LZLiPq55-sc";
+    public static final String VIDEO_ID = "lBRpw31cNe0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        YouTubePlayerView youTubePlayerView = (YouTubePlayerView)findViewById(R.id.youtubeplayerview);
+        youTubePlayerView.initialize(API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider,
+                                        YouTubeInitializationResult result) {
+        Toast.makeText(getApplicationContext(),
+                "onInitializationFailure()",
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player,
+                                        boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo(VIDEO_ID);
+        }
+    }
+
+}

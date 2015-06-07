caf0b4ee5f689b983fd676d254bbc6b20c2ab922
Hernan Maschwitz <hernanm992@gmail.com>
2015-06-07 17:40:31 -0300

    mejoro listado

--- app/src/main/java/com/xapp/naves/xapp/DataSource.java
+++ app/src/main/java/com/xapp/naves/xapp/DataSource.java
@@ -14,11 +14,15 @@ public class DataSource {
 
     static{
 
-        TAREAS.add(new Tarea("Trotar 30 minutos","08:00",R.drawable.ic_health));
-        TAREAS.add(new Tarea("Estudiar análisis técnico","10:00",R.drawable.ic_money));
-        TAREAS.add(new Tarea("Comer 4 rebanadas de manzana","10:30",R.drawable.ic_health));
-        TAREAS.add(new Tarea("Asistir al taller de programación gráfica","15:45",R.drawable.ic_carreer));
-        TAREAS.add(new Tarea("Consignarle a Marta","18:00",R.drawable.ic_money));
+        TAREAS.add(new Tarea("Shuvit","Longboard",R.drawable.shuvit));
+        TAREAS.add(new Tarea("Kickflip","Longboard",R.drawable.kickflip));
+        TAREAS.add(new Tarea("Flat top","Longboard",R.drawable.flat_top));
+        TAREAS.add(new Tarea("Ghostride kickflip","Longboard",R.drawable.ghostride_kickflip));
+        TAREAS.add(new Tarea("Footbrake","Longboard",R.drawable.footbrake));
+        TAREAS.add(new Tarea("Turndown","BMX",R.drawable.turndown));
+        TAREAS.add(new Tarea("Footjam","BMX",R.drawable.footjam));
+        TAREAS.add(new Tarea("360 bunny hop ","BMX",R.drawable.bunny_hop));
+
 
     }
 
